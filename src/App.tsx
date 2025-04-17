86ce2dac502f93cbf219ab398779e676d8ca2475
zedaenious <matt@zelenak.me>
2025-04-17 02:27:09 -0400

    DatePicker & DateTracker components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from 'react';
 import { ThemeProvider } from "@/components/ui/theme-provider"
 import Header from './components/ui/Header';
+import DateTracker from './components/ui/DateTracker';
 
 function App({children}: PropsWithChildren) {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Header />
+      <DateTracker />
       {children}
     </ThemeProvider>
   )
---
+++ src/components/ui/DateTracker.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DatePicker } from './DatePicker'
+
+const DateTracker = () => {
+  return (
+    <section className='flex flex-row justify-center py-10 px-5'>
+      <DatePicker />
+    </section>
+  )
+}
+
+export default DateTracker
